b85d0baf538a9b568db977c8f4f6dde5d18ef935
Muhammad Rafi Fahrezi <110459675+rafifahrezi75@users.noreply.github.com>
2026-05-20 16:58:53 +0700

    cicil admin

---
+++ app/src/main/java/com/example/warmindone/pesananselesai/PesananSelesaiModel.java
@@ -0,0 +1,64 @@
+package com.example.warmindone.pesananselesai;
+
+import com.google.firebase.Timestamp;
+
+public class PesananSelesaiModel {
+
+    private String id_order;
+    private String id_user;
+    private String status;
+    private String metode;
+    private Timestamp tanggal_order;
+    private long total_harga;
+
+    public PesananSelesaiModel() {
+    }
+
+    public String getId_order() {
+        return id_order;
+    }
+
+    public void setId_order(String id_order) {
+        this.id_order = id_order;
+    }
+
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMetode() {
+        return metode;
+    }
+
+    public void setMetode(String metode) {
+        this.metode = metode;
+    }
+
+    public Timestamp getTanggal_order() {
+        return tanggal_order;
+    }
+
+    public void setTanggal_order(Timestamp tanggal_order) {
+        this.tanggal_order = tanggal_order;
+    }
+
+    public long getTotal_harga() {
+        return total_harga;
+    }
+
+    public void setTotal_harga(long total_harga) {
+        this.total_harga = total_harga;
+    }
+}
